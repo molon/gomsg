42874a2459a5ea13f495ab2bb91dff0c615c5655
molon <dudl@qq.com>
2019-06-03 16:19:09 +0800

    - 移除env dev脚本 - auth/config.go -> flag.go

--- script/dev/redis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-docker run -d \
-        --restart=always \
-        --name redis \
-        -p 9379:9379 \
-        -v $(pwd)/store/redis:/data \
-        redis:latest \
-        redis-server --appendonly yes
